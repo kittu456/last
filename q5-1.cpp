cff944fcbff3e268ee2c967c830bb7280b841234
kittu456 <31301120+kittu456@users.noreply.github.com>
2017-11-11 01:02:30 +0530

    Create q5-1.cpp

---
+++ q5-1.cpp
@@ -0,0 +1,16 @@
+include <iostream>
+using namespace std;
+
+class ZooAnimal  
+   {
+    private:
+      char *name;
+      int cageNumber;
+      int weightDate;
+      int weight;
+    public:
+      void Destroy (); // destroy function
+      void Create (char * a,int x,int y,int z);
+      char* reptName ();
+      int daysSinceLastWeighed (int today);
+   };
